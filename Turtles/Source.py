8bb1b2b7ad13b47ddb2ec1466fc3dbff27614161
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-01-15 17:31:40 +0600

    * Simple for loop

--- Turtles/Source.py
+++ Turtles/Source.py
@@ -1,27 +1,31 @@
 import turtle
 import time
-import math
+
+
+def draw_edge(width, bob):
+    for i in range(3):
+        bob.fd(width)
+        bob.lt(90)
+    bob.fd(width)
+
+def draw_roof(width, bob):
+    bob.lt(150)
+    bob.pd()
+    bob.fd(width)
+    bob.rt(120)
+    bob.fd(width)
 
 bob = turtle.Turtle()
 print(bob)
 
 # draw a house
 width = 100
-
-bob.fd(width)
-bob.lt(90)
-bob.fd(width)
-bob.lt(90)
-bob.fd(width)
-bob.lt(90)
-bob.fd(width)
+draw_edge(width, bob)
 bob.pu()
 bob.bk(width)
-bob.lt(150)
-bob.pd()
-bob.fd(width)
-bob.rt(120)
-bob.fd(width)
+draw_roof(width, bob)
+
+
 
 
 time.sleep(30)
