0cbf6069071e54485c35c7782ad12486608d3f1d
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-01-23 12:19:21 +0600

    List - is anagram?

--- List/Source.py
+++ List/Source.py
@@ -1,10 +1,11 @@
 
 
-def chop(t):
-    del t[0]
-    del t[len(t) - 1]
+def is_anagram(s, t):
+    s.sort()
+    t.sort()
+
+    return s == t
 
 
-t = [1,2,3,4]
-chop(t)
-print(t)
+print(is_anagram([3,2,1,4],[1,2,3,4]))
+print(is_anagram([3,2,1,5],[1,2,3,4]))
