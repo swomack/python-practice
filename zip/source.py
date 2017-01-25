593ad6faab8b4c33941a71dcf4bf5a1909176b51
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-01-25 12:49:17 +0600

    * Tuple:  Zip with list and dictionary

--- zip/source.py
+++ zip/source.py
@@ -1,1 +1,26 @@
-print('hello world!')
+
+s = "hello"
+count = [10,20,30,40,50,60]
+dic = dict()
+dic['a'] = 1
+dic['b'] = 2
+
+print(dic.values())
+
+for pair in zip(s, count, dic, dic.values()):
+    print(pair)
+
+t = list(zip(s, count, dic, dic.values()))
+print(t)
+
+
+dic = {'a' : 100, 'b' : 200}
+
+tu = dic.items()
+
+for k, v in tu:
+    print(k ,v)
+
+
+
+
