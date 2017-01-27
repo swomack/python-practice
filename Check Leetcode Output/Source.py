c1b98217901c8b7df7806c8ea43059163b5515af
swomack <sourov0805045@gmail.com>
2017-01-27 16:41:05 +0600

    Check Leetcode output

---
+++ Check Leetcode Output/Source.py
@@ -0,0 +1,5 @@
+
+
+print("First output length:" + str(len(["stow","stob","seer","seen","embow","neem","wene","wave","wadna","reest","reem","renew","rine","rive","riven","riva","inerm","irene","vine","viner","vire","avener","avine","bowl","sise","besa","besan","bend","benda","bena","daven","wots","myst","send","teil","sand","sane","sang","anda","anes","anesis","nane","nanes"])) + "\n")
+
+print("Second output length:" + str(len(["anda","anes","anesis","avener","avine","bena","bend","benda","besa","besan","bowl","daven","embow","inerm","irene","myst","nane","nanes","neem","reem","reest","renew","rine","riva","rive","riven","sand","sane","sang","seen","seer","send","sise","stob","stow","teil","vine","viner","vire","wadna","wave","wene","wots"])) + "\n")
