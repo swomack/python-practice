4edc9f029c67a3545b52e68f57a1a7052a6ab002
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-01-30 15:36:51 +0600

    Updated time.

--- Time/Source.py
+++ Time/Source.py
@@ -6,7 +6,7 @@ class Time:
 
 
 def print_time(time):
-    print("%.2d : %.2d : %.2d" % (time.hour, time.minute, time.minute))
+    print("%.2d : %.2d : %.2d" % (time.hour, time.minute, time.second))
 
 
 def is_after(time1, time2):
@@ -16,22 +16,51 @@ def is_after(time1, time2):
     return total_second1 > total_second2
 
 
+def add_time(time1, time2):
+    res = Time()
+    res.hour = time1.hour + time2.hour
+    res.minute = time1.minute + time2.minute
+    res.second = time1.second + time2.second
 
-time1 = Time()
-time1.hour = 12
-time1.minute = 20
-time1.second = 35
+    res.minute += (res.second // 60)
+    res.second %= 60
 
-print_time(time1)
+    res.hour += int(res.minute // 60)
+    res.minute %= 60
+    
 
+    return res
 
-time2 = Time()
-time2.hour = 13
-time2.minute = 20
-time2.second = 35
-
-print_time(time2)
+def is_same_time(time1, time2):
+    return (time1.hour == time2.hour and time1.minute == time2.minute and time1.second == time2.second)
 
 
 
-print(is_after(time1, time2))
+
+movie_start = Time()
+movie_start.hour = 9
+movie_start.minute = 45
+movie_start.second = 0
+
+print_time(movie_start)
+
+
+movie_end = Time()
+movie_end.hour = 11
+movie_end.minute = 20
+movie_end.second = 0
+
+print_time(movie_end)
+
+print(is_after(movie_end, movie_start))
+
+movie_duration = Time()
+movie_duration.hour = 1
+movie_duration.minute = 35
+movie_duration.second = 0
+
+
+calculated_movie_end = add_time(movie_start, movie_duration)
+print_time(calculated_movie_end)
+
+print(is_same_time(calculated_movie_end, movie_end))
